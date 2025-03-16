75aea8057467a17689f4027766a24e576cbd1968
prochaosdevv <info@himotechglobal.com>
2025-03-16 21:26:27 +0530

    added .env example

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 
-export const API_URL = "http://localhost:3004/api/gundy"
-// export const API_URL = "https://d3homchhswklat.cloudfront.net/pongcoin/api/gundy"
+export const API_URL = process.env.NEXT_APP_API_URL
+ 
